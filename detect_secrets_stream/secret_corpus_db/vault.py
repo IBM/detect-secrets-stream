5661150adcf1d4ae5f9f64cb6c11fc9adf3381b6
Stephen Linnell <sclinnell@ibm.com>
2023-09-13 14:03:03 -0400

    updates from suggestions
    Signed-off-by: Stephen Linnell <sclinnell@ibm.com>

--- detect_secrets_stream/secret_corpus_db/vault.py
+++ detect_secrets_stream/secret_corpus_db/vault.py
@@ -19,9 +19,7 @@ class Vault(BaseVaultBackend):
         global secrets_manager_service
         if os.path.exists(config_file):
             os.environ['IBM_CREDENTIALS_FILE'] = config_file
-
             # begin-common
-
             secrets_manager_service = SecretsManagerV2.new_instance(
             )
             # end-common
@@ -30,12 +28,11 @@ class Vault(BaseVaultBackend):
             # Load the configuration
             global config
             config = read_external_sources(SecretsManagerV2.DEFAULT_SERVICE_NAME)
-        
-    #function to throw issues if there's no configuration available
-    needscredentials = pytest.mark.skipif(
-        not os.path.exists(config_file), reason="External configuration not available, skipping..."
-    )
-    @needscredentials
+#function to throw issues if there's no configuration available
+    # needscredentials = pytest.mark.skipif(
+    #     not os.path.exists(config_file), reason="External configuration not available, skipping..."
+    # )
+    # @needscredentials
     def create_or_update_secret(self, token_id: int, secret: str, other_factors=None):
         """ Creates secret if doesn't exist, updates it if it does.
         Accepts: token_id: int, corresponds with the token_id in database
@@ -49,7 +46,6 @@ class Vault(BaseVaultBackend):
         }
         # Creating a new secret name to ensure theres no collisions in searching
         secret_name = "DSS-"+str(token_id)+"-DSS"
-
         secret_prototype_created = {
                 'description': 'Secret Found in DSS',
                 'labels': ['dss', 'production'],
@@ -63,10 +59,10 @@ class Vault(BaseVaultBackend):
                     secret_prototype=secret_prototype_created,
                 )
             # Creating a response type object to put the status code in 
-            return_response = requests.Response() 
-            return_response.status_code = create_response.status_code
-        except ApiException as e:
-            pytest.fail(str(e))
+            return_response = requests.Response(status_code=create_response.status_code)
+            # return_response.status_code = create_response.status_code
+        except Exception:
+             raise VaultReadException('Error writing secret to Secrets Manager. Something is wrong')
 
         return return_response  
     
@@ -77,7 +73,6 @@ class Vault(BaseVaultBackend):
         Throws: VaultReadException if secret not in vault or other error encountered.
         """
         try:
-            print('\nlist_secrets() result:') 
             # begin-list_secrets to and searching for the tokenID
             all_results = []
             secret_name = "DSS-"+str(token_id)+"-DSS"
@@ -88,27 +83,24 @@ class Vault(BaseVaultBackend):
                 search=secret_name,     # searching for the secret name 
                 groups=['1262ffb2-8921-3442-2e2b-cd35d4a1c838'],
             )
-            while pager.has_next():
-                next_page = pager.get_next()
-                assert next_page is not None
-                all_results.extend(next_page)
-
+            all_results.extend(pager.get_next())
+            if pager.has_next():
+                raise VaultReadException('There was a collision in reading secrets from Secrets manager (more than one found)')
+            
             found_secret = all_results[0] # Testing with naming convention ensures only 1 will return (max)
             ibm_cloud_secret_id = found_secret['id']
         except Exception:
-             raise VaultReadException('Error reading secret from vault. Secret might not be in vault.')
+             raise VaultReadException('Error listing secrets from Secrets Manager. Could be an issue with secret group.')
         
         try:
-            print('\nget_secret() result:')
             # begin-get_secret for the ID we got earlier 
             response = secrets_manager_service.get_secret(
                 id=ibm_cloud_secret_id,
             )
-            secret = response.get_result() 
+            secret = response.get_result()
             # end-get_secret
 
         except Exception:
-             raise VaultReadException('Error reading secret from vault. Secret might not be in vault.')
+            raise VaultReadException('Error reading secret from Secrets Manager. Secret might not be in Secrets Manager.') 
         else:
-            return secret['data']  
-        
+            return secret['data']
